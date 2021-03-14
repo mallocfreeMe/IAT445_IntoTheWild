ab28b31be1cf466640c2b8f786c60f49329b122b
Jianghui Dai <jianghuidai@outlook.com>
2021-03-13 22:45:01 -0800

    fix the merge issue

---
+++ Assets/Unity Asset Store/BOXOPHOBIC/AssetSource3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSource3 : MonoBehaviour
+{
+    /*
+    Asset Title:    FREE Skybox Extended Shader
+    Asset Creator:  BOXOPHOBIC 
+    Asset Link:     https://assetstore.unity.com/packages/vfx/shaders/free-skybox-extended-shader-107400
+
+    */
+}
---
+++ Assets/Unity Asset Store/Standard Assets/AssetSource2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSource2 : MonoBehaviour
+{
+    /*
+    Asset Title:    Standard Assets (for Unity 2018.4)
+    Asset Creator:  Unity Technologies
+    Asset Link:     https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-for-unity-2018-4-32351
+
+    */
+}
