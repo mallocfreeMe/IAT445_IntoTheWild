3439fff22fc4e5fcc830c39153f056266b71dd81
ArtemisMoon <artemismoon89@gmail.com>
2021-03-17 17:45:51 -0700

    Updated player animations & some asset adjustment for making animations

---
+++ Assets/Sound/PlayerAction/Reference.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reference : MonoBehaviour
+{
+    /*
+
+     - Eating Soundeffect: https://freesound.org/people/PaulMorek/sounds/172034/
+
+
+
+    */
+}
