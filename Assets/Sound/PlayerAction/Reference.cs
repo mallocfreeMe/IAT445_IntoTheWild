c32749d1062f4d44e380bcdcb5fdfd0240b28965
ArtemizMoon <artemismoon89@gmail.com>
2021-04-08 14:08:51 -0700

    Opening Animation

--- Assets/Sound/PlayerAction/Reference.cs
+++ Assets/Sound/PlayerAction/Reference.cs
@@ -7,11 +7,36 @@ public class Reference : MonoBehaviour
     /*
 
      - Eating Soundeffect: https://freesound.org/people/Vinrax/sounds/160978/
+
      - Tools swing Soundeffect:  https://freesound.org/people/orangesheepdog/sounds/367182/
-     - Walking on Sand: https://freesound.org/people/fthgurdy/sounds/528953/
-     - Walking on Grass: https://freesound.org/people/JanKoehl/sounds/85603/
+
+     - Walking on Sand: https://www.zapsplat.com/music/footstep-single-flip-flop-on-road-or-track-lightly-covered-with-sand-version-17/
+                        https://www.zapsplat.com/music/footstep-single-flip-flop-on-road-or-track-lightly-covered-with-sand-version-18/
+
+     - Walking on Grass: https://www.zapsplat.com/music/single-footstep-on-short-grass-2/
+                        https://www.zapsplat.com/music/single-footstep-on-short-grass-3/
+                        https://www.zapsplat.com/music/single-footstep-on-short-grass-4/
+
      - Walking on Mud: https://freesound.org/people/EricsSoundschmiede/sounds/454623/
-     - Hurt: https://freesound.org/people/mrickey13/sounds/515624/
+
+     - Plane SFX: https://www.zapsplat.com/music/loop-steady-sound-of-a-ultralight-plane-flying-with-low-speed/
+                  https://www.zapsplat.com/music/loop-steady-sound-of-a-ultralight-plane-flying-with-medium-speed/
+                  https://www.zapsplat.com/music/airfieldsmall-airport-ambience-light-aircraft-queue-up-to-takeoff-1/
+                  https://www.zapsplat.com/music/aircraft-boeing-stearman-b75-biplanes-1940-1945-aircraft-flying-in-distance/
+                  https://www.zapsplat.com/music/gas-cooker-stove-flame-alight-with-short-bursts-as-gas-runs-out-1/
+                  https://www.zapsplat.com/music/punchy-explosion-with-a-short-tail-1/
+                  https://www.zapsplat.com/music/gas-hob-oven-or-bbq-flame-blow-in-wind-version-1/
+                  https://www.zapsplat.com/music/flame-burst-from-gas-pipe-large-and-wide-version-1/
+                  https://www.zapsplat.com/music/car-lights-on-warning-tone-sounds-when-engine-off-but-lights-left-on-and-door-open/
+                  https://www.zapsplat.com/music/emergency-alarm-or-siren-beeping-warning-tone-1/
+                  https://www.zapsplat.com/music/emergency-alarm-or-siren-beeping-warning-tone-2/
+                  https://www.zapsplat.com/music/plane-crash-light-propeller-aircraft-fly-by-fast-then-explosion-exterior-perspective-5/
+                  https://www.zapsplat.com/music/car-radio-static-through-speaker-tuning-through-stations/
+    
+
+     - Opening BGM: https://www.youtube.com/watch?v=1i0643HZjkY&ab_channel=AndrewPustea
+
+
 
 
 
